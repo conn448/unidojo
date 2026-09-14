322af706b3c4e3f5c92e893798619359c5575341
conn448 <connmartin693@gmail.com>
2026-09-14 10:33:39 +0100

    chore: bump service worker cache for retention release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='unidojo-shell-v8';
+const CACHE='unidojo-shell-v9';
 const SHELL=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
